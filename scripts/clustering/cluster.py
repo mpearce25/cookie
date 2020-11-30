75dbfee57b33e0dc0ea412ba694af320fc4c1c34
mpearce25 <pearcematthew25@gmail.com>
2020-11-30 01:51:11 -0500

    clustering all on 1 graph

--- scripts/clustering/cluster.py
+++ scripts/clustering/cluster.py
@@ -5,21 +5,36 @@ from sklearn.cluster import KMeans
 
 # Read in CSV of BLEU scores and parse important columns
 fn = "blue.csv"
-cols = ["victim BLEU (before)", "victim BLEU (after)", "attacker BLEU (before)", "attacker BLEU (after)"]
+cols = ["victim BLEU (before)", "victim BLEU (after)", "attacker BLEU (before)", "attacker BLEU (after)", "% watermarked", "p value"]
 df = pd.read_csv(fn)[cols]
-print(df.head())
+print(df)
 
-# Plot
+# Plot data
 vb, va = df[cols[0]], df[cols[1]]
 ab, aa = df[cols[2]], df[cols[3]]
-vy = [0] * len(vb)
-ay = [1] * len(ab)
+y = df[cols[4]]
 
-plt.scatter(vb, vy, s = 20, label="Victim BLEU Original")
-plt.scatter(va, vy, s  = 20, label="Victim BLEU After Stylized")
-plt.scatter(ab, ay, s = 20, label="Attacker BLEU Original")
-plt.scatter(aa, ay, s = 20, label = "Attacker BLEU 5%")
-plt.legend()
+plt.scatter(vb, y, s = 20, label="Victim BLEU Original")
+plt.scatter(va, y, s  = 20, label="Victim BLEU Stylized")
+plt.scatter(ab, y, s = 20, label="Attacker BLEU Original training")
+plt.scatter(aa, y, s = 20, label = "Attacker BLEU Mixed (Stylized / Original) Training")
+plt.legend(bbox_to_anchor=(.04, -.50), loc='center left')
+plt.title("Victim / Attacker BLEU Scores")
+plt.ylabel("% of Stylized Attacker Training Data ")
+plt.xlabel("BLEU4 Score")
+plt.tight_layout() #tight margins
+plt.yticks(range(0, 25, 5))
+plt.xticks(range(30, 36, 1))
+
+# Calculate means
+ps = [5, 10, 15]
+# 5%
+
+
+# 10%
+
+# 15%
+
 plt.show()
 
 
